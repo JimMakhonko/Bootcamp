56a08e9d7c0b11abb609badfc92a61a6dd1ed444
Ievgenii Makhonko <yobaua@gmail.com>
2022-07-07 14:18:58 +0800

    commit

--- src/Module2/Section7/Dealership.java
+++ src/Module2/Section7/Dealership.java
@@ -26,7 +26,24 @@ public class Dealership {
         this.cars[index].drive();
         this.cars[index] = null;
     }
-
+    /**
+     * Function name: search
+     *
+     * @param make (String)
+     * @param budget (int)
+     * @return (int)
+     *
+     * Inside the function:
+     *   1. Loops through every element in the cars field.
+     *   2. Skips the run if the element is null.
+     *   3. If it finds a car the user wants and can afford:
+     *     - println: \nWe found a car in spot <i> \n\n <toString>
+     *     - print: If you're interested, type 'yes':
+     *     - returns the index
+     *   4. If the loop runs and it didn't find a car <------
+     *     - println: \nYour search didn't match any results.\n
+     *     - returns 404
+     */
     public int search(String make, int budget) {
         for (int i = 0; i < cars.length; i++) {
 
@@ -45,19 +62,6 @@ public class Dealership {
         }
         return 0;
     }
-
-/*
-    public String search(String make, int budget) {
-        for (int i = 0; i < this.cars.length; i++) {
-            if (this.cars[i] == null) {
-                continue;
-            } else if (this.cars[i].getMake().equals(make) && this.cars[i].getPrice() <= budget) {
-                return "\nWe found one in spot " + i + "\n" + this.cars[i].toString() + "\nAre you interested?";
-            }
-        }
-        return "Sorry, we couldn't find any cars according to your requirements.";
-    }
-*/
 
     public String toString() {
         String temp = "";
